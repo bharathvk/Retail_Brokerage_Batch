6aedf0844cac37a886afda6aa82fa947fdd6b5e4
bharathvk <89610005+bharathvk@users.noreply.github.com>
2024-09-25 22:30:29 -0700

    Update src_data_generator.sh

--- src_data_generator.sh
+++ src_data_generator.sh
@@ -2,6 +2,6 @@
 cd /
 mkdir ~/TPC-DI
 mkdir ~/TPC_DI_SRC_DATA
-tar -xvf /2D826240-256F-4B24-9930-9E758119E430-TPC-DI-Tool -C /TPC-DI
+tar -xvf /TPC-DI-Tool-Orig -C /TPC-DI
 cd /TPC-DI/Tools 
 java -jar DIGen.jar -o /TPC_DI_SRC_DATA -sf 5
